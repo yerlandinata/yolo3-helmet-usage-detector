e2a757cfd992d99fad15770a24899322e96365b6
Yudhistira Erlandinata <yudhistira.erlandinata@ui.ac.id>
2018-09-01 00:10:17 +0700

    fix wrong filename to upload

--- train.sh
+++ train.sh
@@ -3,6 +3,6 @@
 ls .gdrive && \
 python3 train.py && \
 epochTime=$(date +%s) && \
-mv ./logs/001/trained_weights_stage_1.h5 ./training_${epochTime}.h5 && \
+mv ./logs/001/trained_weights_final.h5 ./training_${epochTime}.h5 && \
 drive -c .gdrive upload --file training_${epochTime}.h5 && \
 echo success && sleep 86400
