43a8971bbad12c412c7eecc97ef1444d7074c6f0
Yudhistira Erlandinata <yudhistira.erlandinata@ui.ac.id>
2018-08-31 23:08:17 +0700

    update train.sh script

--- train.sh
+++ train.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-drive
-python train.py
-epochTime=$(date +%s)
-mv ./logs/001/trained_weights_stage_1.h5 ./training_${epochTime}.h5 
-drive upload --file training_${epochTime}.h5 
+drive && \
+python3 train.py && \
+epochTime=$(date +%s) && \
+mv ./logs/001/trained_weights_stage_1.h5 ./training_${epochTime}.h5 && \
+drive upload --file training_${epochTime}.h5 && \
+echo success
