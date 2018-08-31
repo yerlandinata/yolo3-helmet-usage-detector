9920cdc450b033c7a9bf4113c50df790ab286572
Yudhistira Erlandinata <yudhistira.erlandinata@ui.ac.id>
2018-08-31 23:55:49 +0700

    fix gdrive upload command

--- train.sh
+++ train.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 ls .gdrive && \
-drive -c .gdrive && \
 python3 train.py && \
 epochTime=$(date +%s) && \
 mv ./logs/001/trained_weights_stage_1.h5 ./training_${epochTime}.h5 && \
-drive upload --file training_${epochTime}.h5 && \
+drive -c .gdrive upload --file training_${epochTime}.h5 && \
 echo success
