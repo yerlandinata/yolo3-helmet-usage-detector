bdabbfb811fa33bdaede78d26e180e26179fd807
Yudhistira Erlandinata <yudhistira.erlandinata@ui.ac.id>
2018-08-31 23:57:14 +0700

    add sleep after training

--- train.sh
+++ train.sh
@@ -5,4 +5,4 @@ python3 train.py && \
 epochTime=$(date +%s) && \
 mv ./logs/001/trained_weights_stage_1.h5 ./training_${epochTime}.h5 && \
 drive -c .gdrive upload --file training_${epochTime}.h5 && \
-echo success
+echo success && sleep 86400
