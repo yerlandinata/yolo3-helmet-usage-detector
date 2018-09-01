f67b7fcd1c468ee3865ea5c890b46b7550b24482
Yudhistira Erlandinata <yudhistira.erlandinata@ui.ac.id>
2018-09-01 10:48:06 +0700

    update train.sh to sleep no matter what

--- train.sh
+++ train.sh
@@ -5,4 +5,5 @@ python3 train.py && \
 epochTime=$(date +%s) && \
 mv ./logs/001/trained_weights_final.h5 ./training_${epochTime}.h5 && \
 drive -c .gdrive upload --file training_${epochTime}.h5 && \
-echo success && sleep 86400
+echo success
+sleep 86400
